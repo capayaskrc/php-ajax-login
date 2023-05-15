f7aeb010a35082c874426c5402cc086c3e3b6661
Kayl Rovic Capayas <kaylrovic.capayas@student.dmmmsu.edu.ph>
2023-05-15 13:17:32 +0800

    working_login

--- template/footer.php
+++ template/footer.php
@@ -1,11 +1,21 @@
-<div class="divider"></div>
-		<h1>YOUR FOOTER</h1>
-  </body>
+
+
+
+
+<footer class="text-center mt-5">
+  <p>All rights reserved. &copy;2023 IGP SYSTEM</p>
+</footer>
 </html>
+
+
+
+
+
 
 
 <!-- this is your js file that you included on  index.php -->
 
 <?php foreach($js as $jsFile ){ ?>
    <script type="text/javascript" src="<?php echo $jsFile ?>"></script>
+   
 <?php }?>
